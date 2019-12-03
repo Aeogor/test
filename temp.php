0f49d308fd5a56dd37171712b3031cb5d9d8f15c
Srinivas Lingutla <aeogorbill@gmail.com>
2019-12-03 14:55:37 -0700

    Update temp.php

--- temp.php
+++ temp.php
@@ -1,14 +1,3 @@
 <?php
-
-function shutdown() {
-    if (!is_null(error_get_last())) {
-        http_response_code(200);
-    }
-}
-register_shutdown_function('shutdown');
-
-header("HTTP/1.1 200 OK");
-error_reporting(0);
-ini_set('display_errors', 'off');
 echo "Hello";
 ?>
